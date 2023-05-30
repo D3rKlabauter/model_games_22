3d511cddf1755f3900e4b51ecf931734841b8895
Ed <68585996+D3rKlabauter@users.noreply.github.com>
2023-05-30 15:18:23 +0200

    interacting

--- Assets/Game/Scripts/AvoidPlayer.cs
+++ Assets/Game/Scripts/AvoidPlayer.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class AvoidPlayer : MonoBehaviour
 {
     public Transform player;
-    public float detectionRadius = 5f;
-    public float moveSpeed = 20f;
+    public float detectionRadius = 50f;
+    public float moveSpeed = 5f;
+    public float approachSpeed = 5f;
 
     private bool isMoving = false;
     private Vector3 moveDirection;
@@ -14,18 +15,30 @@ public class AvoidPlayer : MonoBehaviour
         // Check if the player is within the detection radius
         if (Vector3.Distance(transform.position, player.position) <= detectionRadius)
         {
-            if (!isMoving)
+            // Check if the player is moving
+            if (player.GetComponent<Rigidbody>().velocity.magnitude <= 0.1f)
             {
-                // Generate a random direction away from the player
-                moveDirection = (transform.position - player.position).normalized;
-                moveDirection += Random.insideUnitSphere;
-                moveDirection.Normalize();
+                if (!isMoving)
+                {
+                    // Generate a random direction away from the player
+                    moveDirection = (transform.position - player.position).normalized;
+                    moveDirection += Random.insideUnitSphere;
+                    moveDirection.Normalize();
 
-                isMoving = true;
+                    isMoving = true;
+                }
+
+                // Move the object in the calculated direction
+                transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
             }
+            else
+            {
+                // Calculate the direction towards the player
+                moveDirection = (player.position - transform.position).normalized;
 
-            // Move the object in the calculated direction
-            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+                // Move the object towards the player
+                transform.Translate(moveDirection * approachSpeed * Time.deltaTime);
+            }
         }
         else
         {
