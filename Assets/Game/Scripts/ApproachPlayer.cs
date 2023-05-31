9f4f518699b2b0823768e06f0050f0b9a5eeeeb9
Ed <68585996+D3rKlabauter@users.noreply.github.com>
2023-05-31 16:41:25 +0200

    ApproachPlayer

---
+++ Assets/Game/Scripts/ApproachPlayer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ApproachPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float detectionRadius = 50f;
+    public float moveSpeed = 5f;
+    public float approachSpeed = 5f;
+
+    private Vector3 moveDirection;
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if the player is within the detection radius
+        if (Vector3.Distance(transform.position, player.position) <= detectionRadius)
+        {
+            // Calculate the direction towards the player
+            moveDirection = (player.position - transform.position).normalized;
+
+            // Move the object towards the player
+            rb.AddForce(moveDirection * approachSpeed);
+
+         }
+        else
+        {
+            // Generate a random direction away from the player
+            moveDirection = (transform.position - player.position).normalized;
+            moveDirection += Random.insideUnitSphere;
+            moveDirection.Normalize();
+
+
+            // Move the object in the calculated direction
+            rb.AddForce(moveDirection * moveSpeed);
+        }
+    }
+}
